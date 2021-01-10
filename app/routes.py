73ab4298d330847c252134b3d2db58dddbeef5ea
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2021-01-09 16:56:27 -0800

    Added total cart value and cleaned up window

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,19 @@
 from flask import render_template, request
 from app import app
 
+total_cart_value = 0
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-    print(request)
+    global total_cart_value
     itemPrice = 5
     if request.method == 'POST':
-        result = float(request.form['quantity'])
-        print(result)
+        result = int(request.form['quantity'])
+        total_cart_value += result
     else:
         result = 0
 
-    site_info = {'store_name': 'TEST', 'bundle_name': 'TEST', 'bundle_description': "Dont stress about tonight's "
+    site_info = {'store_name': 'Asian Grocery', 'bundle_name': 'TEST', 'bundle_description': "Don't stress about tonight's "
                                                                                     "winter dinner! Friends and "
                                                                                     "family can snuggle up to the "
                                                                                     "warmth of this mouthwatering "
@@ -23,4 +24,5 @@ def index():
                                                                                     "fresh vegetables and Ichiran "
                                                                                     "ramen, tonight's dinner hassle "
                                                                                     "is covered by us!"}
-    return render_template('indexStyles.html', result=result*itemPrice, site_info=site_info)
+    bullet_points = ["lamb", "pork", "beef slices", "fresh vegetables", "Ichiran ramen"]
+    return render_template('indexStyles.html', total_cart_value=total_cart_value, itemPrice=itemPrice, site_info=site_info, bullet_points=bullet_points)
